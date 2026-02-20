66ec3c67bb60c15ca21c1a1f58eefc59d6cf14ce
aimkick <aimkick@hotmail.com>
2026-02-20 19:28:48 +0800

    Initial Commit

---
+++ PaodekuaiNative/PaodekuaiNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaodekuaiNativeApp.swift
+//  PaodekuaiNative
+//
+//  Created by jeioc on 2026/2/20.
+//
+
+import SwiftUI
+
+@main
+struct PaodekuaiNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
